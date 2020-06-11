01d64ef038bb03e982858e065db65353daa6ec20
Eliel Sousa <sousaeliel@gmail.com>
2020-06-11 19:04:41 -0300

    Adicionando um pouco de segurança com https e helmet

--- src/api/libs/boot.js
+++ src/api/libs/boot.js
@@ -1,11 +1,21 @@
+import fs from "fs";
+import https from "https";
+
 module.exports = app => {
     const ENV = app.get("env");
     console.log(`Task Manager environment '${ENV}'`)
 
     if (ENV === "test") return;
-    
+
+    const credentials = {
+        key: fs.readFileSync("taskmanager.key", "utf8"),
+        cert: fs.readFileSync("taskmanager.cert", "utf8"),
+    };
+
     const PORT = app.get("port");
+
     app.db.sequelize.sync().done(() => {
-        app.listen(PORT, () => console.log(`Task Manager listen on port '${PORT}'`));
+        https.createServer(credentials, app)
+            .listen(PORT, () => console.log(`Task Manager listen on port '${PORT}'`));
     });
 };
--- src/api/libs/middlewares.js
+++ src/api/libs/middlewares.js
@@ -5,10 +5,24 @@ import logger from "./logger";
 import morgan from "morgan";
 import cluster from "cluster";
 import compression from "compression";
+import helmet from "helmet";
 
 module.exports = app => {
     app.set("port", 3000);
     app.set("json spaces", 4);
+    app.set("env", !process.env.NODE_ENV ? "development" : process.env.NODE_ENV);
+
+    app.use(helmet());
+    app.use(compression());
+    app.use(bodyParser.json());
+    app.use(app.auth.initialize());
+    app.use(express.static("public"));
+
+    app.use(cors({
+        origin: [ "http://localhost:3001" ],
+        methods: [ "GET", "POST", "PUT", "DELETE" ],
+        allowedHeaders: [ "Content-Type", "Authorization" ]
+    }));
     
     app.use(morgan("common", {
         stream: {
@@ -18,20 +32,13 @@ module.exports = app => {
         }
     }));
 
-    app.use(cors({
-        origin: [ "http://localhost:3001" ],
-        methods: [ "GET", "POST", "PUT", "DELETE" ],
-        allowedHeaders: [ "Content-Type", "Authorization" ]
-    }));
-    
-    app.use(compression());
-    app.use(bodyParser.json());
-    app.use(app.auth.initialize());
-    app.use(express.static("public"));
-    app.set("env", !process.env.NODE_ENV ? "development" : process.env.NODE_ENV);
-
-    app.use((req, _res, next) => {
+    app.use((req, res, next) => {
         delete req.body.id;
         next();
     });
 };
+
+//res.removeHeader("X-Powered-By");
+//app.set('x-powered-by', false); 
+//app.disable('x-powered-by');  //express 4.*
+//res.header("X-powered-by", "Blood, sweat, and tears.");
